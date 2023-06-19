1e70f40e3490064e8b1f902ad3c7c38312a11504
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-06-19 18:49:05 +0200

    Home Controller

---
+++ src/main/java/com/example/todolistspringbootintership/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.todolistspringbootintership.controllers;
+
+import com.example.todolistspringbootintership.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController
+{
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping(value = {"/", "/home"})
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("Item", itemService.getAll());
+        return modelAndView;
+    }
+}
