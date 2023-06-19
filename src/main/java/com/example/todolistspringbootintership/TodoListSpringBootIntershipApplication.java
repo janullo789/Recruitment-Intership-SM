df90622a72fb1b592a3f028ff1459194c1418712
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-06-19 17:04:19 +0200

    Init spring-boot app

---
+++ src/main/java/com/example/todolistspringbootintership/TodoListSpringBootIntershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistspringbootintership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringBootIntershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListSpringBootIntershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todolistspringbootintership/TodoListSpringBootIntershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistspringbootintership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListSpringBootIntershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
